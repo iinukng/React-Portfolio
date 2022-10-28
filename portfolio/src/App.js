d0b899e42a2d58d136a6070a13d4551a42c42631
Justin Kang <iinukng@gmail.com>
2022-10-27 23:38:42 -0400

    functional react portfolio template

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import { Home } from './components/Home';
 import { Portfolio } from './components/Portfolio';
 import { Contact } from './components/Contact';
 import { Resume } from './components/Resume';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer'
+// import { Footer } from './components/Footer'
 
 function App () {
   return (
@@ -17,8 +18,9 @@ function App () {
       <Route path='contact' element={< Contact />} />
       <Route path='resume' element={< Resume />} />
     </Routes>
+    <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
